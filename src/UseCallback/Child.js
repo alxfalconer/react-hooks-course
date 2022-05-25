83c511a2201d2980e6a00286e818245ef79b2f54
Alexander <al.x.falconer@gmail.com>
2022-05-25 14:00:00 +0100

    callback complete

--- src/UseCallback/Child.js
+++ src/UseCallback/Child.js
@@ -1,11 +1,9 @@
 import React, { useEffect } from "react";
 
-function Child({ returnComment }) {
-  useEffect(() => {
-    console.log("FUNCTION WAS CALLED");
-  }, [returnComment]);
+function Child({ returnComment }) 
+useEffect(() => {
+  console.log("FUNCTION WAS CALLED");
+}, [returnComment]);
 
-  return <div>{returnComment("Pedro")}</div>;
-}
-
+return <div>{returnComment("Pedro")}</div>;
 export default Child;
--- src/UseContext/Login.js
+++ src/UseContext/Login.js
@@ -2,17 +2,7 @@ import React, { useContext } from "react";
 import { AppContext } from "./ContextTutorial";
 
 function Login() {
-  const { setUsername } = useContext(AppContext);
 
-  return (
-    <div>
-      <input
-        onChange={(event) => {
-          setUsername(event.target.value);
-        }}
-      />
-    </div>
-  );
 }
 
 export default Login;
