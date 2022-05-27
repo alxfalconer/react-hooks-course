44dec880ee28c9d1d1593a6307bd9ff18a3ef2fb
Alexander <al.x.falconer@gmail.com>
2022-05-27 14:00:00 +0100

    updated App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import CallbackTutorial from "./UseCallback/CallbackTutorial";
+import EffectTutorial from "./UseEffect/EffectTutorial";
+import ContextTutorial from "./UseContext/ContextTutorial";
 
 function App() {
   return (
     <div className="App">
     <CallbackTutorial />
+    <EffectTutorial />
+    <ContextTutorial />
     </div>
   );
 }
