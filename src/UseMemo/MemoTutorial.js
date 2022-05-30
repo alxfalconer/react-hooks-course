29f3d4fbebcb2d429f882a700ef1fa3f4b3f2d1c
Alexander <al.x.falconer@gmail.com>
2022-05-30 14:00:00 +0100

    memo 1st try

--- src/UseMemo/MemoTutorial.js
+++ src/UseMemo/MemoTutorial.js
@@ -2,5 +2,19 @@ import axios from "axios";
 import { useEffect, useState, useMemo } from "react";
 
 export default function MemoTutorial() {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        setData(response.data);
+      });
+  }, []);
+
+  const findLongestName = (comments) => {
+    if (!comments) return null;
+  }
 
 }
