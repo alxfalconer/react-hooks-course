f9f422339ab48f0d9ea4d04ed9bc82c8a4c91a25
Alexander <al.x.falconer@gmail.com>
2022-06-06 14:00:00 +0100

    reducer 1st commit

--- src/UseReducer/ReducerTutorial.js
+++ src/UseReducer/ReducerTutorial.js
@@ -1,6 +1,16 @@
 import React, { useReducer } from "react";
 
 const reducer = (state, action) => {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return { count: state.count + 1, showText: state.showText };
+      case "toggleShowText":
+        return { count: state.count, showText: !state.showText };
+      default:
+        return state;
+    }
+  };
 
 };
 
