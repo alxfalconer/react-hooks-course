929dd65f7470f717eb248ee6db374e5f365d4b8b
Alexander <al.x.falconer@gmail.com>
2022-06-06 14:00:00 +0100

    1st state

--- src/UseState/StateTutorial.js
+++ src/UseState/StateTutorial.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
 
 const StateTutorial = () => {
-  
+  const [inputValue, setInputValue] = useState("Pedro");
+
+  let onChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+  };
+
 };
 
 export default StateTutorial;
